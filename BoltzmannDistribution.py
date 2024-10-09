3a2a4c0bba94fbc9de9219f1c285cd689cc8e99f
James Rogers <JamesMRogers@gmail.com>
2024-10-08 20:10:20 -0400

    Update BoltzmannDistribution.py
    Updated the documentation

--- BoltzmannDistribution.py
+++ BoltzmannDistribution.py
@@ -4,78 +4,199 @@ from scipy.optimize import fsolve
 from collections import OrderedDict
 import json
 import os
-
-# Speed of light in m/s
-c = 299792458  
+from typing import Dict, Union
+from pathlib import Path
 
 class BoltzmannDistribution:
-    def __init__(self):
+    """
+    A class representing the Boltzmann distribution through a single representative point.
+    This class provides methods to calculate various properties of the distribution at
+    different temperatures.
+    
+    Attributes:
+        SPEED_OF_LIGHT (float): Speed of light in m/s
+        representative_point (float): The 50% point at 5000K reference temperature
+        reference_temperature (float): Reference temperature in Kelvin
+        cache_size (int): Maximum number of entries in the energy ratio cache
+        cache (OrderedDict): LRU cache for energy ratios
+        cache_path (Path): Path to the cache file
+    """
+    
+    SPEED_OF_LIGHT = 299792458  # m/s
+
+    def __init__(self, 
+                 reference_temperature: float = 5000,
+                 cache_size: int = 10000,
+                 cache_filename: str = "boltzmann_cache.json"):
+        """
+        Initialize the BoltzmannDistribution instance.
+
+        Args:
+            reference_temperature (float): Reference temperature in Kelvin
+            cache_size (int): Maximum number of entries in the cache
+            cache_filename (str): Name of the file to store the cache
+        """
         self.representative_point = 4.7849648084645400e-20  # 50% point at 5000K
-        self.reference_temperature = 5000
-        self.cache_size = 10000
-        self.cache = OrderedDict()
-        self.cache_filename = "boltzmann_cache.json"
-        self.load_cache()
+        self.reference_temperature = reference_temperature
+        self.cache_size = cache_size
+        self.cache: OrderedDict = OrderedDict()
+        self.cache_path = Path(cache_filename)
+        self._load_cache()
 
-    def scale_to_temperature(self, T):
-        return T / self.reference_temperature
+    def scale_to_temperature(self, temperature: float) -> float:
+        """
+        Scale a value from reference temperature to target temperature.
 
-    def point_energy(self, T):
-        return self.representative_point * self.scale_to_temperature(T)
+        Args:
+            temperature (float): Target temperature in Kelvin
 
-    def peak_energy(self, T):
-        return self.point_energy(T) / 0.2457097414250071665
+        Returns:
+            float: Scaling factor
+        """
+        return temperature / self.reference_temperature
 
-    def area_under_curve(self, T):
-        return self.point_energy(T) * T * 10e-22/50.20444590190353665093425661
+    def point_energy(self, temperature: float) -> float:
+        """
+        Calculate the point energy at a given temperature.
 
-    def peak_frequency(self, T):
-        return self.point_energy(T) * 10e32 / .1627836661598892
+        Args:
+            temperature (float): Temperature in Kelvin
 
-    def fwhm(self, T):
-        return self.point_energy(T)/.162935865000977884
+        Returns:
+            float: Point energy
+        """
+        return self.representative_point * self.scale_to_temperature(temperature)
 
-    def wavelength_from_frequency(self, frequency):
-        return c / frequency
+    def peak_energy(self, temperature: float) -> float:
+        """
+        Calculate the peak energy of the distribution at a given temperature.
 
-    def frequency_from_wavelength(self, wavelength):
-        return c / wavelength
+        Args:
+            temperature (float): Temperature in Kelvin
 
-    def energy_at_percentage(self, T, percentage):
+        Returns:
+            float: Peak energy
+        """
+        return self.point_energy(temperature) / 0.2457097414250071665
+
+    def area_under_curve(self, temperature: float) -> float:
+        """
+        Calculate the area under the distribution curve at a given temperature.
+
+        Args:
+            temperature (float): Temperature in Kelvin
+
+        Returns:
+            float: Area under the curve
+        """
+        return (self.point_energy(temperature) * temperature * 
+                10e-22 / 50.20444590190353665093425661)
+
+    def peak_frequency(self, temperature: float) -> float:
+        """
+        Calculate the peak frequency of the distribution at a given temperature.
+
+        Args:
+            temperature (float): Temperature in Kelvin
+
+        Returns:
+            float: Peak frequency
+        """
+        return self.point_energy(temperature) * 10e32 / 0.1627836661598892
+
+    def fwhm(self, temperature: float) -> float:
+        """
+        Calculate the Full Width at Half Maximum (FWHM) at a given temperature.
+
+        Args:
+            temperature (float): Temperature in Kelvin
+
+        Returns:
+            float: FWHM value
+        """
+        return self.point_energy(temperature) / 0.162935865000977884
+
+    @staticmethod
+    def wavelength_from_frequency(frequency: float) -> float:
+        """
+        Convert frequency to wavelength using the speed of light.
+
+        Args:
+            frequency (float): Frequency in Hz
+
+        Returns:
+            float: Wavelength in meters
+        """
+        return BoltzmannDistribution.SPEED_OF_LIGHT / frequency
+
+    @staticmethod
+    def frequency_from_wavelength(wavelength: float) -> float:
+        """
+        Convert wavelength to frequency using the speed of light.
+
+        Args:
+            wavelength (float): Wavelength in meters
+
+        Returns:
+            float: Frequency in Hz
+        """
+        return BoltzmannDistribution.SPEED_OF_LIGHT / wavelength
+
+    def energy_at_percentage(self, temperature: float, percentage: float) -> float:
+        """
+        Calculate the energy at a given percentage of the distribution.
+
+        Args:
+            temperature (float): Temperature in Kelvin
+            percentage (float): Percentage point to calculate (between 0 and 1)
+
+        Returns:
+            float: Energy at the specified percentage
+        """
         k = constants.Boltzmann
+        
         def equation(E):
-            return np.exp(-E / (k * T)) - percentage
-        return fsolve(equation, k*T)[0]
+            return np.exp(-E / (k * temperature)) - percentage
+        
+        initial_guess = k * temperature
+        return float(fsolve(equation, initial_guess)[0])
 
-    def calculate_energy_ratio(self, percentage):
-        # Calculate energy at the reference temperature (5000K)
-        return self.energy_at_percentage(self.reference_temperature, percentage)
+    def get_energy_ratio(self, percentage: float, temperature: float) -> float:
+        """
+        Get the energy ratio for a given percentage and temperature, using caching.
 
-    def get_energy_ratio(self, percentage, temperature):
-        # Check if percentage is in cache
+        Args:
+            percentage (float): Percentage point to calculate
+            temperature (float): Temperature in Kelvin
+
+        Returns:
+            float: Energy ratio
+        """
         if percentage in self.cache:
             reference_ratio = self.cache[percentage]
-            self.cache.move_to_end(percentage)  # Move to end to mark as recently used
+            self.cache.move_to_end(percentage)
         else:
-            reference_ratio = self.calculate_energy_ratio(percentage)
+            reference_ratio = self.energy_at_percentage(
+                self.reference_temperature, percentage)
             self.cache[percentage] = reference_ratio
             if len(self.cache) > self.cache_size:
-                self.cache.popitem(last=False)  # Remove least recently used item
+                self.cache.popitem(last=False)
 
-        # Scale to requested temperature and return
         return reference_ratio * self.scale_to_temperature(temperature)
 
-    def load_cache(self):
-        if os.path.exists(self.cache_filename):
-            with open(self.cache_filename, 'r') as f:
+    def _load_cache(self) -> None:
+        """Load the cache from disk if it exists."""
+        if self.cache_path.exists():
+            with self.cache_path.open('r') as f:
                 loaded_cache = json.load(f)
-                self.cache = OrderedDict((float(k), v) for k, v in loaded_cache.items())
+                self.cache = OrderedDict(
+                    (float(k), v) for k, v in loaded_cache.items())
 
-    def save_cache(self):
-        with open(self.cache_filename, 'w') as f:
+    def save_cache(self) -> None:
+        """Save the current cache to disk."""
+        with self.cache_path.open('w') as f:
             json.dump({str(k): v for k, v in self.cache.items()}, f)
 
     def __del__(self):
+        """Save the cache when the object is destroyed."""
         self.save_cache()
-
-
